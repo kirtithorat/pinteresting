7633a4ac809ecf952c6e8737b3dcd1a28aecbcdf
kirtithorat <kirti.brenz@gmail.com>
2014-01-25 17:06:23 -0600

    Created Raw HTML files for the views

--- db/migrate/20140125191228_create_pins.rb
+++ db/migrate/20140125191228_create_pins.rb
@@ -1,7 +1,6 @@
 class CreatePins < ActiveRecord::Migration
   def change
     create_table :pins do |t|
-      t.string :name
       t.text :description
       t.attachment :image
       t.references :boards, index: true
