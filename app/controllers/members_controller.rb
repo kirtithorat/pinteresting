a08d12711e5f83abb2c6bd48ad5597e4fd1ab2ff
kirtithorat <kirti.brenz@gmail.com>
2014-02-13 15:00:16 -0600

    Added error page,for scenario when a member looks for a non-existing member dashboard page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -8,7 +8,11 @@ class MembersController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def load_dashboard
     if current_member.nil?
-      @member = Member.find(params[:id])
+      begin
+        @member = Member.find(params[:id])
+      rescue
+        render :errors and return
+      end
     else
       @member = current_member
     end
