42ecb4f32f2beadf954ad32e73770af8069e26db
kirtithorat <kirti.brenz@gmail.com>
2014-02-13 09:41:20 -0600

    Added thumbnail style to Member Avatar and included Pins Count on Boards index partial

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,6 @@
 class Board < ActiveRecord::Base
   belongs_to :member
-  has_many :pins
+  has_many :pins, dependent: :destroy
   validates :name, presence: true, uniqueness: {scope: :member_id}
   validates :category, :member_id, presence: true
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,12 +3,12 @@ class Member < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
-  has_many :boards
-  has_many :pins, through: :boards
+  has_many :boards, dependent: :destroy
 
   validates :firstname, :lastname, :location ,  presence: true
 
   has_attached_file :avatar,
+    :styles => { :thumb => "100x100>" },
     :path => '/Users/kirti/Dropbox/Projects/RubyonRails/pinteresting/spec/support/uploads/:class/:attachment/:id/:basename.:extension',
     :url => '/spec/support/uploads/:class/:attachment/:id/:basename.:extension'
 
